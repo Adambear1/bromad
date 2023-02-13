8252e9acf7393010a7ecf48bf0fd5fa31b9aac7b
adambear1 <adamcarlbirgenheier@gmail.com>
2023-02-13 09:10:21 -0600

    push

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,21 +9,30 @@ function About() {
         <p className='text-center'>
           <strong className='text-left'>About the Bromad:</strong>
           {"  "}
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat temporibus enim eveniet, corporis sapiente, maxime hic fugit totam similique quae quisquam? Placeat sequi possimus ipsa consequatur quidem consectetur ut ipsam.
-                                                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat temporibus enim eveniet, corporis sapiente, maxime hic fugit totam similique quae quisquam? Placeat sequi possimus ipsa consequatur quidem consectetur ut ipsam.
-                                                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat temporibus enim eveniet, corporis sapiente, maxime hic fugit totam similique quae quisquam? Placeat sequi possimus ipsa consequatur quidem consectetur ut ipsam.
+          From investing, business development, personal freedom, and overall experience,
+          this unique twist on Tim Ferriss' "4 Hour Work Week" mixed with modern day
+          Nomadism, this philosophy allows you to get the most in life. Traveling
+          to Latin America, living abroad in Eastern Europe, or roaming the foothills in Asia,
+          the goal in life is to be free and to leave your footmark in the family airloom.
         </p>
         <p className='text-center'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat temporibus enim eveniet, corporis sapiente, maxime hic fugit totam similique quae quisquam? Placeat sequi possimus ipsa consequatur quidem consectetur ut ipsam.
+          Sadly, today many people fall into the 9-5 trap. They believe they need to work to 
+          have the coolest car, the biggest house, or the most unbelievable life. I know people with 
+          8-figures and I know people with $8 and unfortunately the underlying fallacy that lie with
+          each is that money somehow defines who we are. I disagree. The experience and mindset of an 
+          individual defines who they are. When I started this journey I had less than a $1,000 to my name.
+          The entire time I was mentally and morally rich though. I had bright thoughts, knew this wasn't the 
+          life I wanted to live, and made extreme sacrifices to build a platform to get on the path I am on.
+          Working numerous jobs simultaneously, taking 25 credits a quarter in college, and living off PB&J 
+          sandwiches for a year I learned how to concept of money. Money is nothing but a means to an end. To live
+          the Bromad lifestyle the main focal points needs to be how money works for you.
         </p>
         <br/>
-        <Quote quote={"a random motivational quote about something."}
-          author={"Yes A. Sir"}/>
+        <Quote quote={"Go where you are treated best."}
+          author={"Andrew Henderson - The Nomad Capitalist"}/>
         <br/>
         <p className='text-center'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi ipsam architecto nam repellat cupiditate natus vero obcaecati, itaque recusandae quaerat quia quibusdam, fuga atque dolor odio, quasi sint eius eaque!
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi ipsam architecto nam repellat cupiditate natus vero obcaecati, itaque recusandae quaerat quia quibusdam, fuga atque dolor odio, quasi sint eius eaque!
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi ipsam architecto nam repellat cupiditate natus vero obcaecati, itaque recusandae quaerat quia quibusdam, fuga atque dolor odio, quasi sint eius eaque!
+          
         </p>
       </div>
     </>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -18,19 +18,25 @@ function Home() {
     {
       src: desertSunset,
       content: "Travel",
-      info: "The Bromad believes in traveling the world which propels one in life to get out of one's comfort zone, explore new places, create meaningful experiences and to build and invest to help those around you. This is the core pillar of the Bromad mentality."
+      info: `The Bromad believes in traveling the world which propels one in life to get out of one's comfort zone, explore new places, create meaningful experiences and to build and invest to help those around you. This is the core pillar of the Bromad mentality. All succeeding factors are a faction of the traveling attribute. Experience is garnered when one travels. Exploring is means of traveling. Creating and building
+      is done best as the end of traveling. Tasting is the emotion that comes from traveling: new food, new cultures, new experience. Investing is maximized from traveling.`
     },
     {
       src: wine,
-      content: "Experience"
+      content: "Experience",
+      info: "Experience is the propellant in life. It's what helps take you to the next level both mentally and physically. It is the true satisfaction one gets from it in turn for being on the right path. Living the Bromad lifestyle means that you'll always garner new experiences. It means you'll be living out of your comfort zone and continually being in shock of 'Holy Sxxt! I can't believe this is actually happening.'"
     },
     {
       src: mountains,
-      content: "Explore"
+      content: "Explore",
+      info:`Exploring is the means to experience, both literally and figuratively. One thing the Bromad philosophy preaches is to always push yourself and try new things. 'Pushing' oneself isn't to be conflated with working hard and doing extreme things. In fact, quite the opposite. 'Pushing' means to mentally, spiritually, and emotionally explore the confines of yourself and take it a step further. An example would be with daily journaling.
+      I try to journal every day, but in reality I journal 3-5 times a week. During these sessions I explore what I am capable of and what I can do right now to take the next steps in the journey. These mental, spiritual, and emotional sessions help me put the next pieces to the puzzle when finding out what I am capable of. I am exploring the future to mold with the present.`
     },
     {
       src: coding,
-      content: "Create"
+      content: "Create",
+      info: `To be a Bromad you need to have the ability to create. Create opportunity, create jobs, create a lifestyle. If you reflect and acknowledge you have this power, but haven't actually created anything worth mentioning yet. That is fine. This social dichotomy we're born in the "fortunate" United States and western society is one of the biggest and most crippling fallacies we could face.
+      Here, we're told that we're either unfortunately part of the 99% and we'll never be anything. Or that we do well enough and that we're entitled assholes who needs the give everything back to society.`
     }, {
       src: ocean,
       content: "Build"
